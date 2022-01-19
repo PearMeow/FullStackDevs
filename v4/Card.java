470e4fa00299441839efa1dfb6d8ece40bed32d4
Salaj Rijal <srijal30@stuy.edu>
2022-01-19 15:58:47 -0500

    yessir

--- v4/Card.java
+++ v4/Card.java
@@ -1,11 +1,10 @@
 public class Card implements Comparable {
 
-    public static String[] suitArray = new String[] {"Diamonds", "Clubs", "Hearts", "Spades"};
-    public static String[] numArray = new String [] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     private int number;  // 6, 7, 8, 9, 10, 11 (J), 12 (Q), 13 (K), 14 (A)
     private int suit;    // 0 = diamonds, 1 = clubs, 2 = hearts, 3 = spades
 
-
+    public static String[] suitArray = new String[] {"Diamonds", "Clubs", "Hearts", "Spades"};
+    public static String[] numArray = new String [] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     public static int trump; 
 
 
@@ -31,20 +30,18 @@ public class Card implements Comparable {
             //raise error
             return;
         }
-	
-	//base cases
-        if (!perry.isTrump() && this.isTrump){
-		return 1;
-	}
+	    //base cases
+            if (!perry.isTrump() && this.isTrump){
+	    	return 1;
+	    }
 
-	else if (perry.isTrump() && !this.isTrump){
-		return -1;
-	}
-	
-	else{
-		int difference = this.number - perry.number;
-		return (difference)/Math.abs(difference); 
-	}
+	    else if (perry.isTrump() && !this.isTrump){
+	    	return -1;
+	    }
+    
+	    else{
+	    	int difference = this.number - perry.number;
+	    	return (difference)/Math.abs(difference); 
+	    }
     }
-
 }
--- v4/Deck.java
+++ v4/Deck.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 
 public class Deck {
 
-    public static void shuffle( ArrayList al )
-  {
+  public static void shuffle( ArrayList al ){
     int randomIndex;
     //setup for traversal fr right to left
     for( int i = al.size()-1; i > 0; i-- ) {
@@ -14,37 +13,30 @@ public class Deck {
     }
   }
 
-    public static void draw(ArrayList<Card> decker, ArrayList<Card> handy){
-        handy.add(decker
-        decker.remove(0);
-    }
-
-    public static ArrayList<Card> creator() {
-        ArrayList<Card> deck = new ArrayList<Card>();
-        for(int i = 0; i < 4; i++){
-            for(int t = 6; t < 15; t++){
-                deck.add(new Card(i, t));
-            }
-        }
-        return deck;
-    }
-
-    public static void main (String[] args) {
-        ArrayList<Card> theDeck = creator();
-        shuffle(theDeck);
-        
-        
-        System.out.println(theDeck);
-        
-
-        ArrayList<Card> p1Hand = new ArrayList<Card>();
-        ArrayList<Card> p2Hand = new ArrayList<Card>();
-
-        //code that hands out cards
-
-        Card trumpCard = theDeck.get(0);
-        Card.trump = trumpCard.getSuit();
-
-    }
-
+  public static void draw(ArrayList<Card> decker, ArrayList<Card> handy){
+      handy.add(decker.get(0));
+      decker.remove(0);
+  }
+  public static ArrayList<Card> creator() {
+      ArrayList<Card> deck = new ArrayList<Card>();
+      for(int i = 0; i < 4; i++){
+          for(int t = 6; t < 15; t++){
+              deck.add(new Card(i, t));
+          }
+      }
+      return deck;
+  }
+  public static void main (String[] args) {
+      ArrayList<Card> theDeck = creator();
+      shuffle(theDeck);
+      
+      
+      System.out.println(theDeck);
+      
+      ArrayList<Card> p1Hand = new ArrayList<Card>();
+      ArrayList<Card> p2Hand = new ArrayList<Card>();
+      //code that hands out cards
+      Card trumpCard = theDeck.get(0);
+      Card.trump = trumpCard.getSuit();
+  }
 }
