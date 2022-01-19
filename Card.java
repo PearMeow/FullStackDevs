32d8f624465fbcbd84944a7cbb167d313503428b
PearMeow <phuang30@stuy.edu>
2022-01-18 21:42:13 -0500

    hi

--- Card.java
+++ Card.java
@@ -1,6 +1,6 @@
 public class Card implements Comparable {
 
-    private int number;  // 6, 7,8,9,10,11 (J), 12(Q), 13(K), 14(A)
+    private int number;  // 6, 7, 8, 9, 10, 11 (J), 12 (Q), 13 (K), 14 (A)
     private int suit;    // 1 = diamond, 2 = clubs, 3 = hearts, 4 = spades
     public static int trump; 
 
@@ -12,8 +12,8 @@ public class Card implements Comparable {
 
     }
 
-    public int compareTo (Comparable perry) {
-        
+    public int compareTo (Object perry) {
+        return 0;
     }
 
     public String toString(){
